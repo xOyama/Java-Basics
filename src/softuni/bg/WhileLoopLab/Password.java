cbd11a32b8fec9ca7ec9110809d3f221d55d5ba8
xOyama <paberukun@gmail.com>
2020-12-02 01:26:12 +0200

    ezpez

---
+++ src/softuni/bg/WhileLoopLab/Password.java
@@ -0,0 +1,17 @@
+package softuni.bg.WhileLoopLab;
+import java.util.Scanner;
+public class Password {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String userName = sc.nextLine();
+        String password = sc.nextLine();
+        String input = sc.nextLine();
+
+        while (!input.equals(password)){
+            input = sc.nextLine();
+        }
+        System.out.printf("Welcome %s!", userName);
+
+    }
+}
