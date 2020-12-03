10a6dd0506a4959d01f90fd368796ca54aa03c0d
xOyama <paberukun@gmail.com>
2020-12-04 00:23:23 +0200

    123456

---
+++ src/softuni/bg/WhileLoopExercise/Vacation.java
@@ -0,0 +1,45 @@
+package softuni.bg.WhileLoopExercise;
+import java.util.Scanner;
+public class Vacation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double neededMoney = Double.parseDouble(sc.nextLine());
+        double ownedMoney = Double.parseDouble(sc.nextLine());
+
+        int daysCounter = 0;
+        int spendingCounter = 0;
+
+        while (ownedMoney < neededMoney && spendingCounter <5){
+            String command = sc.nextLine();
+            double money = Double.parseDouble(sc.nextLine());
+            daysCounter++;
+            if("save".equals(command)){
+                ownedMoney += money;
+                spendingCounter =0;
+            }else if ("spend".equals(command)){
+                ownedMoney -= money;
+                spendingCounter +=1;
+                if (ownedMoney < 0){
+                    ownedMoney = 0;
+                }
+            }
+
+
+        }
+        if (spendingCounter == 5){
+            System.out.println("You can't save the money.");
+            System.out.println(daysCounter);
+        }
+        if (ownedMoney >= neededMoney){
+            System.out.printf("You saved the money for %d days.", daysCounter);
+        }
+
+
+
+
+
+
+
+    }
+}
